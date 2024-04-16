b1f92ba2eb7b274fee5511cef3fc798ce6830b5f
Sanjay Sukhwani <sanjay.silvertouch@gmail.com>
2024-04-16 16:24:34 +0530

    Added the routes of the application in the app_routes.dart

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,9 @@
+// ----------------------------
+// Created By : Sanjay Sukhwani
+// -----------------------------
+class AppRoutes{
+  static const String homePage = "/home";
+  static const String defaultPage = "/";
+  static const String onboardingScreen = "/onboardscreen";
+  static const String loginPage = "/login";
+}

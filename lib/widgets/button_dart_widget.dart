80310437030db00858029842d912bd9667de9aaa
Sanjay Sukhwani <sanjay.silvertouch@gmail.com>
2024-04-16 16:31:01 +0530

    Other Changes : Wrapped Button with Container for Height & Width

--- lib/widgets/button_dart_widget.dart
+++ lib/widgets/button_dart_widget.dart
@@ -3,22 +3,33 @@ import 'package:flutter/material.dart';
 class DarkButton extends StatelessWidget {
   final double radius;
   final Text text;
+  final double buttonWidth, buttonHeight;
   final VoidCallback actions;
-  const DarkButton({super.key, required this.radius, required this.text,required this.actions});
+
+  const DarkButton(
+      {super.key,
+      required this.radius,
+      required this.text,
+      required this.actions,
+      required this.buttonHeight,
+      required this.buttonWidth});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    // Wrapped with the SizeBox to Add the Height and width of the Button
+    return Container(
+      width: buttonWidth,
+      height: buttonHeight,
+      child: TextButton(
         onPressed: actions,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.black,
-          textStyle: const TextStyle(color: Colors.white),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(radius)
-          )
-
-        ),
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.black,
+            elevation: 10,
+            textStyle: const TextStyle(color: Colors.white),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(radius))),
         child: text,
+      ),
     );
   }
 }

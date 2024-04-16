e098f6a3b01f6fb2abfb1a26bd1ffcd125d66058
Sanjay Sukhwani <sanjay.silvertouch@gmail.com>
2024-04-16 16:26:33 +0530

    Moved all the Strings in one file strings.dart

---
+++ lib/utils/strings.dart
@@ -0,0 +1,31 @@
+class StringResources {
+
+  static const appname = "SnapShop";
+
+  // Image Path for the App Icon
+  static const appIconPath = "lib/assets/icons/app_icon.png";
+
+  // Image Path for the Splash Screen Images
+  static const splashScreenPaths = [
+    "lib/assets/Images/onboarding_image1.png",
+    "lib/assets/Images/onboarding_image2.png",
+    "lib/assets/Images/onboarding_image3.png",
+  ];
+
+  // Strings for the Titles in Splash Screen
+  static const splashScreenTitles = [
+    "Discover Our New Collection",
+    "Order your Style",
+    "",
+  ];
+
+  // Strings for the SubTitles in Splash Screen
+  static const splashScreenSubTitles = [
+    "Easy shopping for all your needs just in hand, trusted by millions of people in the world.",
+    "More than a thousand of our bags are available for your luxury",
+    "",
+  ];
+
+  // Strings for the Next button Text in Splash Screen
+  static const splashScreenNextBtnText = ["Next", "Get Started", "Get Started"];
+}
